dfb1c6205e3cd98bccecd3fae8d4552dac6572d6
Amanda Soares <amandchen@hotmail.com>
2022-10-19 10:49:41 -0300

    Post Domain: cria diretório para o domínio posts. Em posts/models/ cria PostModel

---
+++ src/posts/models/post.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: [String],
+    required: true,
+  },
+  insertedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: [Date],
+  },
+});
+
+const PostModel = model('Post', postSchema);
+
+module.exports = { PostModel };

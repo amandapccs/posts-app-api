2a31f11874dc34d8d504d802e60823a2c0e1df80
Amanda Soares <amandchen@hotmail.com>
2022-10-19 10:50:43 -0300

    src/db: Cria seeder.js para realizar seed inicial do banco de dados

---
+++ src/db/seeder.js
@@ -0,0 +1,34 @@
+const { PostModel } = require('../posts/models/post.model');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function seed() {
+  await mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log('connected to mongodb');
+
+  const posts = [
+    {
+      title: 'Hello World',
+      content: 'This is my first post',
+    },
+    {
+      title: 'Hello World 2',
+      content: 'This is my second post',
+    },
+  ];
+
+  try {
+    await PostModel.insertMany(posts);
+    console.log('posts created');
+  } catch (error) {
+    console.log('Erro no processo de seeding:', error);
+  }
+
+  await mongoose.connection.close();
+}
+
+seed();

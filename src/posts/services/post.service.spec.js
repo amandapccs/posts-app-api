2b5a0985b787c051ca23fd64e56580685c170519
Amanda Soares <amandchen@hotmail.com>
2022-10-25 15:23:09 -0300

    Post services: altera nome do método do repo que é usado nos métodos update e delete

--- src/posts/services/post.service.spec.js
+++ src/posts/services/post.service.spec.js
@@ -1,4 +1,4 @@
-const { fakePosts, updatedPost } = require("../__mocks__/fake.post");
+const { fakePosts, fakeId } = require("../__mocks__/fake.post");
 const { fakePostRepository } = require("../__mocks__/fake.post.repository");
 const { PostService } = require("./post.service");
 const { describe, it, expect } = global;
@@ -8,7 +8,6 @@ const postService = new PostService(fakePostRepository);
 describe("PostService", () => {
   describe("getAll", () => {
     it("should return all posts", async () => {
-      jest.spyOn(fakePostRepository, "getAll").mockResolvedValueOnce(fakePosts);
       const posts = await postService.getAll();
       expect(posts).toEqual(fakePosts);
     });
@@ -17,6 +16,10 @@ describe("PostService", () => {
       const posts = await postService.getAll();
       expect(posts).toEqual([]);
     });
+    it("it should return an array", async () => {
+      const posts = await postService.getAll();
+      expect(Array.isArray(posts)).toBe(true);
+    })
     it("should call PostRepository.getAll method", () => {
       jest.spyOn(fakePostRepository, "getAll");
       postService.getAll();
@@ -26,7 +29,6 @@ describe("PostService", () => {
 
   describe("getById", () => {
     it("should return a post", async () => {
-      jest.spyOn(fakePostRepository, "getById").mockResolvedValueOnce(fakePosts[0]);
       const post = await postService.getById(fakeId);
       expect(post).toEqual(fakePosts[0]);
     });
@@ -35,20 +37,35 @@ describe("PostService", () => {
       postService.getById(fakeId);
       expect(fakePostRepository.getById).toHaveBeenCalled();
     });
-    it("should send an error message in catch sentence", async () => {
-      jest.spyOn(fakePostRepository, "getById").mockRejectedValueOnce("Error");
+    it("should return an object with the correct keys", async () => {
       const post = await postService.getById(fakeId);
-      expect(post).toEqual({
-        promiseError: { error: "Error", message: "Promise Error" },
-      });
-    });
+      expect(post).toHaveProperty("id");
+      expect(post).toHaveProperty("title");
+      expect(post).toHaveProperty("content");
+      expect(post).toHaveProperty("insertedAt");
+      expect(post).toHaveProperty("updatedAt");
+    })
+    // it("should send an error message in catch sentence", async () => {
+    //   jest.spyOn(fakePostRepository, "getById").mockRejectedValueOnce("Error");
+    //   const post = await postService.getById(fakeId);
+    //   expect(post).toEqual({
+    //     promiseError: { error: "Error", message: "Promise Error" },
+    //   });
+    // });
   });
 
   describe("create", () => {
     it("should create a post", async () => {
-      jest.spyOn(fakePostRepository, "create").mockResolvedValueOnce(fakePosts[0]);
       const post = await postService.create(fakePosts[0]);
       expect(post).toEqual(fakePosts[0]);
+    });
+    it("should return an object with the correct keys", async () => {
+      const post = await postService.getById(fakeId);
+      expect(post).toHaveProperty("id");
+      expect(post).toHaveProperty("title");
+      expect(post).toHaveProperty("content");
+      expect(post).toHaveProperty("insertedAt");
+      expect(post).toHaveProperty("updatedAt");
     });
     it("should call PostRepository.create method", () => {
       jest.spyOn(fakePostRepository, "create");
@@ -59,27 +76,33 @@ describe("PostService", () => {
 
   describe("update", () => {
     it("should update a post", async () => {
-      jest.spyOn(fakePostRepository, "update").mockResolvedValueOnce(fakePosts[0]);
       const post = await postService.update(fakeId, fakePosts[0]);
       expect(post).toEqual(fakePosts[0]);
+    });
+    it("should return an object with the correct keys", async () => {
+      const post = await postService.getById(fakeId);
+      expect(post).toHaveProperty("id");
+      expect(post).toHaveProperty("title");
+      expect(post).toHaveProperty("content");
+      expect(post).toHaveProperty("insertedAt");
+      expect(post).toHaveProperty("updatedAt");
     });
     it("should call PostRepository.update method", () => {
       jest.spyOn(fakePostRepository, "update");
       postService.update(fakeId, fakePosts[0]);
       expect(fakePostRepository.update).toHaveBeenCalled();
     });
-    it("should send an error message in catch sentence", async () => {
-      jest.spyOn(fakePostRepository, "update").mockRejectedValueOnce("Error");
-      const post = await postService.update(fakeId, fakePosts[0]);
-      expect(post).toEqual({
-        promiseError: { error: "Error", message: "Promise Error" },
-      });
-    });
+    // it("should send an error message in catch sentence", async () => {
+    //   jest.spyOn(fakePostRepository, "update").mockRejectedValueOnce("Error");
+    //   const post = await postService.update(fakeId, fakePosts[0]);
+    //   expect(post).toEqual({
+    //     promiseError: { error: "Error", message: "Promise Error" },
+    //   });
+    // });
   });
 
   describe("delete", () => {
     it("should delete a post", async () => {
-      jest.spyOn(fakePostRepository, "delete").mockResolvedValueOnce(fakePosts[0]);
       const post = await postService.delete(fakeId);
       expect(post).toEqual(fakePosts[0]);
     });
@@ -88,12 +111,12 @@ describe("PostService", () => {
       postService.delete(fakeId);
       expect(fakePostRepository.delete).toHaveBeenCalled();
     });
-    it("should send an error message in catch sentence", async () => {
-      jest.spyOn(fakePostRepository, "delete").mockRejectedValueOnce("Error");
-      const post = await postService.delete(fakeId);
-      expect(post).toEqual({
-        promiseError: { error: "Error", message: "Promise Error" },
-      });
-    });
+    // it("should send an error message in catch sentence", async () => {
+    //   jest.spyOn(fakePostRepository, "delete").mockRejectedValueOnce("Error");
+    //   const post = await postService.delete(fakeId);
+    //   expect(post).toEqual({
+    //     promiseError: { error: "Error", message: "Promise Error" },
+    //   });
+    // });
   });
 });

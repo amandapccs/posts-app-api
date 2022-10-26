a918bfca79f90d2b6b7838fbb6427a0691f2dec8
Amanda Soares <amandchen@hotmail.com>
2022-10-26 15:54:02 -0300

    Post Controllers: adiciona lógica de lidar com erros às rotas

--- src/posts/controllers/post.controller.js
+++ src/posts/controllers/post.controller.js
@@ -6,66 +6,55 @@ class PostController {
     this.service = service;
   }
 
-  async getAll(req, res) {
-    try {
-      const posts = await this.service.getAll();
-      res.status(StatusCode.OK).json(posts);
-    } catch (error) {
-      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(error);
-    }
+  async getAll(_req, res) {
+    const posts = await this.service.getAll();
+    if ('validationError' in posts) return res.status(posts.validationError.status).json({ message: posts.validationError.message });
+    return res.status(StatusCode.OK).json(posts);
   }
 
   async getById(req, res) {
-    try {
-      const { id } = req.params;
-      const post = await this.service.getById(id);
-      res.status(StatusCode.OK).json(post);
-    } catch (error) {
-      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(error);
-    }
+    const { id } = req.params;
+    const post = await this.service.getById(id);
+    if ('validationError' in post) return res.status(post.validationError.status).json({ message: post.validationError.message });
+    return res.status(StatusCode.OK).json(post);
   }
 
   async create(req, res) {
-    try {
-      const rawBody = req.apiGateway.event.body;
+    const rawBody = req.apiGateway.event.body;
 
-      if (!isJson(rawBody)) {
-        res.status(StatusCode.BAD_REQUEST).json("Invalid JSON");
-        return;
-      }
-
-      const post = await this.service.create(JSON.parse(rawBody));
-      res.status(StatusCode.CREATED).json(post);
-
-    } catch (error) {
-      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(error);
+    if (!isJson(rawBody)) {
+      res.status(StatusCode.BAD_REQUEST).json("Invalid JSON");
+      return;
     }
+
+    const post = await this.service.create(JSON.parse(rawBody));
+    if ('validationError' in post) return res.status(post.validationError.status).json({ message: post.validationError.message });
+    
+    return res.status(StatusCode.CREATED).json(post);
   }
 
   async update(req, res) {
-    try {
-      const { id } = req.params;
-      const rawBody = req.apiGateway.event.body;
+    const { id } = req.params;
+    const rawBody = req.apiGateway.event.body;
 
-      if (!isJson(rawBody)) {
-        res.status(StatusCode.BAD_REQUEST).json("Invalid JSON");
-        return;
-      }
-      const post = await this.service.update(id, JSON.parse(rawBody));
-      res.status(StatusCode.OK).json(post);
-    } catch (error) {
-      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(error);
+    if (!isJson(rawBody)) {
+      res.status(StatusCode.BAD_REQUEST).json("Invalid JSON");
+      return;
     }
+
+    const post = await this.service.update(id, JSON.parse(rawBody));
+    if ('validationError' in post) return res.status(post.validationError.status).json({ message: post.validationError.message });
+
+    return res.status(StatusCode.OK).json(post);
   }
 
   async delete(req, res) {
-    try {
-      const { id } = req.params;
-      const post = await this.service.delete(id);
-      res.status(StatusCode.OK).json(post);
-    } catch (error) {
-      res.status(StatusCode.INTERNAL_SERVER_ERROR).json(error);
-    }
+    const { id } = req.params;
+    const post = await this.service.delete(id);
+
+    if ('validationError' in post) return res.status(post.validationError.status).json({ message: post.validationError.message });
+      
+    return res.status(StatusCode.OK).json(post);
   }
 }
 

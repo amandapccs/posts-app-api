d5d52c549f47f58523bba6fc5a539475585cf427
Amanda Soares <amandchen@hotmail.com>
2022-10-31 23:27:26 -0300

    app use cors

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({ origin: true }));
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header(

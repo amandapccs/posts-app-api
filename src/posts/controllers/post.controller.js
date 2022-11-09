eef6a05c3d0439936ddb1fac120535907e8761b6
Amanda Soares <amandchen@hotmail.com>
2022-11-08 22:29:33 -0300

    Feat(post.controller.js(: Adiciona DTO ao controller

--- src/posts/controllers/post.controller.js
+++ src/posts/controllers/post.controller.js
@@ -1,5 +1,6 @@
 const { StatusCode } = require("./../../utils/status.code");
 const { isJson } = require("./../../utils/json.validator");
+const { PostDto } = require("../dtos/post.dto");
 
 class PostController {
   constructor(service) {
@@ -8,7 +9,6 @@ class PostController {
 
   async getAll(_req, res) {
     const posts = await this.service.getAll();
-    if ('validationError' in posts) return res.status(posts.validationError.status).json({ message: posts.validationError.message });
     return res.status(StatusCode.OK).json(posts);
   }
 
@@ -21,11 +21,14 @@ class PostController {
 
   async create(req, res) {
     const rawBody = req.apiGateway.event.body;
-    const isBodyJson = isJson(rawBody);
-
-    const body = isBodyJson ? JSON.parse(rawBody) : rawBody;
-
-    const post = await this.service.create(body);
+    
+    if (!isJson(rawBody)) {
+      res.status(StatusCode.BAD_REQUEST).json("Invalid JSON");
+      return;
+    }
+    
+    const postDto = new PostDto(rawBody);
+    const post = await this.service.create(postDto);
     if ('validationError' in post) return res.status(post.validationError.status).json({ message: post.validationError.message });
     
     return res.status(StatusCode.CREATED).json(post);
@@ -39,8 +42,8 @@ class PostController {
       res.status(StatusCode.BAD_REQUEST).json("Invalid JSON");
       return;
     }
-
-    const post = await this.service.update(id, JSON.parse(rawBody));
+    const postDto = new PostDto(rawBody);
+    const post = await this.service.update(id, postDto);
     if ('validationError' in post) return res.status(post.validationError.status).json({ message: post.validationError.message });
 
     return res.status(StatusCode.OK).json(post);
